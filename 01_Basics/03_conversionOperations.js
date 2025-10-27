debdac67c3608a70cd7e808777d8788ccbc794b2
Ekshta Mishra <134239554+ekxhta@users.noreply.github.com>
2025-10-27 18:24:41 +0000

    Conversions in javascript

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -1,10 +1,30 @@
 let score="33abs"
-console.log(typeof score);
+console.log("for string to number : ",typeof score);
 let valueInNumber=Number(score)
 console.log(typeof (valueInNumber));
 console.log(valueInNumber); 
 //since 33abs cannot be converted into number, 
 //but also there is lack of strict checking in javascript- it returns NaN- not a number
 
+/* for conversion to Number
+"33" => 33
+"33abs" =>NaN
+true => 1; false=> 0
+ */
 
+
+let isLoggedIn=1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log("for isLoggedIn: ",booleanIsLoggedIn);
+
+/* 
+1 => true; 0=> falase
+"" => false
+"anything* => true
+*/
+
+let num=33
+let stringNum = String(num)
+console.log("for num to string :",stringNum);
+console.log(typeof stringNum);
 
