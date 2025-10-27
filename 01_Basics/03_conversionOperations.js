04aaca7042013f11e1289d084babd09f1ef67c9f
Ekshta Mishra <134239554+ekxhta@users.noreply.github.com>
2025-10-27 18:09:39 +0000

    conversions in javascript

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,10 @@
+let score="33abs"
+console.log(typeof score);
+let valueInNumber=Number(score)
+console.log(typeof (valueInNumber));
+console.log(valueInNumber); 
+//since 33abs cannot be converted into number, 
+//but also there is lack of strict checking in javascript- it returns NaN- not a number
+
+
+
