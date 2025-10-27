efdc904b9df7f975100775cc7e53049a9be7ed67
Ekshta Mishra <134239554+ekxhta@users.noreply.github.com>
2025-10-27 18:02:07 +0000

    datatypes in javascript

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //instruction that specifies to treat all JS code as newer version
+
+// alert("hello") we cannot use alert, sice we are using nodejs rn, and not the browser console.
+
+let name = "ekxhta"
+let age=18
+let isLogged=false
+
+/*PRIMITIVE DATATYPES:
+-number
+-bigint
+-string =>""
+-boolean => true/false
+-null => standalone value as well as a datatype
+-undefined
+-symbol => for uniques 
+-object*/
+
+/*Null vs undefined => 
+    undefined is when a variable has been decalred without having a value assigned to it.
+    null is an empty value. differentiates absence of a value */
+
+
+console.log(typeof "ekxhta");
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof age);
+
+
+
